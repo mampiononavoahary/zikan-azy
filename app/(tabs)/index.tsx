396ce419a755ad446b36a5993f8ff3e4d650252c
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-03-13 18:38:29 +0300

    feat: call components in index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,15 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import className from 'twrnc'
 import SearchInput from '@/components/SearchInput'
+import PlayAndShuffle from '@/components/PlayAndShuffle'
+import Tracks from '@/components/Tracks'
 
 const index = () => {
   return (
-    <View style={className`flex-1 bg-black`}>
+    <View style={className`flex-1 bg-black gap-5`}>
       <SearchInput />
+      <PlayAndShuffle />
+      <Tracks />
     </View>
   )
 }

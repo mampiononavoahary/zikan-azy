7a11b1adcd58a4f6c82846f1ff62a286effe7386
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-03-14 21:51:29 +0300

    feat: add onpresse pressable

--- components/Track.tsx
+++ components/Track.tsx
@@ -2,12 +2,13 @@ import { View, Text, Image, Pressable } from 'react-native'
 import React from 'react'
 import className from 'twrnc'
 import { Ellipsis } from 'lucide-react-native';
+import MusicScreen from './MusicScreen';
 
-const Track = ({image,title,name}) => {
+const Track = ({image,title,name,onpresse}) => {
   return (
-    <View style={className`flex-row gap-3 justify-between items-center mb-2`}>
+     <View style={className`flex-row gap-3 justify-between items-center mb-2`}>
       <Image source={image} style={className`h-12 w-12 border border-gray-300 rounded-lg`} />
-      <Pressable style={className`flex-1 pb-3`}>
+      <Pressable onPress={onpresse} style={className`flex-1 pb-3`}>
         <Text style={className`text-white text-lg`}>
         {title} 
         </Text>

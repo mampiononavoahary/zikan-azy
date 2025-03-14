03d3da3a8f91c8162203b031138c83befd3b53b3
mampiononavoahary <hei.zoarisoa@gmail.com>
2025-03-14 21:52:39 +0300

    feat: add two usestate to close and show Music Screen

--- components/Tracks.tsx
+++ components/Tracks.tsx
@@ -1,20 +1,33 @@
 import { View, Text, ScrollView, FlatList } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import data from '../assets/data/library.json'
 import Track from './Track'
 import className from 'twrnc'
+import MusicScreen from './MusicScreen'
 
 const Tracks = () => {
+  const [popupVisible,setPopupVisible] = useState(false);
+  const [MusicScreenHide,setMusicScreenHide] = useState(true);
+  const toggleHide = ()=>{
+    setPopupVisible(!popupVisible)
+
+  }
+  const togglePopup = ()=>{
+    setPopupVisible(!popupVisible)
+  }
   return (
+    <>
     <View style={className`flex-1`}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <FlatList data={data}
           renderItem={({ item }) => (
-            <Track image={item.artwork} title={item.title} name={item.artist}/>
+            <Track onpresse={togglePopup} image={item.artwork} title={item.title} name={item.artist}/>
           )}
         />
       </ScrollView>
     </View>
+    {popupVisible && (<MusicScreen onpress={toggleHide}/>)}
+</>
   )
 }
 
